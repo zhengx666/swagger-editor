85655320e31e3365dd9aab17ea02c82431ca64f0
zhengx <313478450@qq.com>
2017-11-27 17:24:09 +0800

    增加koa静态服务器

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by zhengx on 2017/11/27.
+ * 使用koa部署静态资源服务器
+ */
+const Koa = require('koa')
+const path = require('path')
+const static = require('koa-static')
+
+const app = new Koa()
+
+// 静态资源目录对于相对入口文件index.js的路径
+const staticPath = './dist'
+
+app.use(static(
+    path.join( __dirname,  staticPath)
+))
+
+
+app.use( async ( ctx ) => {
+    ctx.body = 'hello world'
+})
+
+app.listen(8080, () => {
+    console.log('[demo] static-use-middleware is starting at port 8080')
+})
